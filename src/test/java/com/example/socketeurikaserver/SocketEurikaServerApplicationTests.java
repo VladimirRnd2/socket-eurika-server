500890ccdd760ba9c95b3679f797faa9b17ede12
vvoronkov <vvoronkov@zuzex.com>
2023-02-28 13:53:32 +0300

    Initial commit

---
+++ src/test/java/com/example/socketeurikaserver/SocketEurikaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socketeurikaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketEurikaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
